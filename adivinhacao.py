5f0146247f8fed3493e1a43aa578c56fc9cbe281
felipeam10 <felipeam10@hotmail.com>
2022-03-18 01:06:23 -0300

    first commit

---
+++ adivinhacao.py
@@ -0,0 +1,28 @@
+print("*********************************")
+print("Bem vindo ao jogo de adivinhacao!")
+print("*********************************")
+
+numero_secreto = 43
+
+chute_str  = input("Digite o seu numero: ")
+
+print("Você digitou ", chute_str)
+
+chute = int(chute_str)
+
+acertou =   chute == numero_secreto
+maior   =   chute > numero_secreto
+menor   =   chute < numero_secreto
+if(acertou):
+    print("Você acertou!")
+else:
+    if(maior):
+        print("Você errou! O seu chute foi maior do que o número secreto")
+    elif(menor):
+        print("Você errou! O seu chute foi menor do que o número secreto")
+print("Fim do Jogo")
+
+
+
+
+
